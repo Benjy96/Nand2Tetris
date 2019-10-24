80ef3d5b381e971d44171e9c2252b56d1a4771d5
Benjy96 <benjamindickey@hotmail.co.uk>
2019-10-24 22:21:16 +0100

    Add input scanning loop to screen filling script

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,15 +12,31 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
-/*
+//
 
-Pseudocode to turn the screen black when a key is pressed on the keyboard:
+//Pseudocode to turn the screen black when a key is pressed on the keyboard:
 
-Loop
-	Set addr = Keyboard Register
-	If Register != 0:
-		iterations = 0
-		Loop while (addr of screen register MAX) > iterations:
-			set current screen register to 1
-			count++
-*/
+//Loop
+//	Set addr = Keyboard Register
+//	If Register != 0:
+//		iterations = 0
+//		Loop while (addr of screen register MAX) > iterations:
+//			set current screen register to 1
+//			count++
+//
+
+(SCAN_LOOP)
+	@KBD
+	D=M
+	@SCAN_LOOP
+	D;JEQ		//if D==0, scan again, else continue to draw loop and fill screen 
+
+	(DRAW_LOOP)	//TODO: Implement.
+		@SCAN_LOOP
+		0;JMP	//Loops back to the start to prevent infinite continued execution
+		
+
+
+
+
+
