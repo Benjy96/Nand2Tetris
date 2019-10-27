df12802dcbc833eaf1daf7ae4c01ea73b3516afc
Benjy96 <benjamindickey@hotmail.co.uk>
2019-10-27 16:03:57 +0000

    Fill screen black upon keypress
    Still to implement: unset the black screen if the user lets go of the keyboard.
    
    The way I will do this is the opposite of the draw loop - I will set the screen registers to white AFTER the draw loop, in any case.  The only difference is that in each iteration of the scan loop - which will set the registers back to white - it will have to break and start the draw loop if there is a keypress.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,39 +1,75 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
-
-// Put your code here.
-//
-
-//Pseudocode to turn the screen black when a key is pressed on the keyboard:
-
-//Loop
-//	Set addr = Keyboard Register
-//	If Register != 0:
-//		iterations = 0
-//		Loop while (addr of screen register MAX) > iterations:
-//			set current screen register to 1
-//			count++
-//
-
 (SCAN_LOOP)
 	@KBD
 	D=M
-	@SCAN_LOOP
-	D;JEQ		//if D==0, scan again, else continue to draw loop and fill screen 
+	
+	//TODO here: Loop to set white if not pressed
 
-	(DRAW_LOOP)	//TODO: Implement.
-		@SCAN_LOOP
-		0;JMP	//Loops back to the start to prevent infinite continued execution
+	//if keyboard not pressed, scan again, else continue to draw loop and fill screen 
+	@SCAN_LOOP
+	D;JEQ			
+
+	@8191
+	D=A
+	@numScreenRegisters	//Number of draw loop iterations required
+	M=D			
+
+	@iteration
+	M=0			
+
+	(DRAW_LOOP)
+		//Multiply 16, "current iteration" many times
+		@registerIncrement
+		M=0
+		@timesRegisterIncremented
+		M=0
+		(INCREMENT_REGISTER)
+			//If iteration == 0, skip loop
+			@iteration
+			D=M
+			@SET_SCREEN
+			D;JEQ
+
+			//Or if iteration == times to set, skip loop
+			@timesRegisterIncremented
+			D=M
+			@iteration
+			D=M-D
+			@SET_SCREEN
+			D;JEQ
+
+			//Else, set register position += 1
+			@1
+			D=A
+			@registerIncrement
+			M=M+D
+
+			//Count num times incremented register and continue
+			@timesRegisterIncremented
+			M=M+1
+			@INCREMENT_REGISTER
+			0;JMP
+		
+		(SET_SCREEN)
+			//Set current register to all black
+			@SCREEN
+			D=A
+			@registerIncrement
+			D=D+M
+			A=D
+			M=-1
+			
+			@iteration	
+			M=M+1
+			D=M
+
+			//If not filled screen yet, continue loop
+			@numScreenRegisters
+			D=M-D		//D = numScreenRegisters - iteration
+			@SCAN_LOOP	//If iterated through all registers, exit draw loop
+			D;JEQ
+
+			@DRAW_LOOP
+			0;JMP		//Continue filling screen
 		
 
 
