d057a8c4f5511987c3ca3c0fd1bc80b24e2237bc
Benjy96 <benjamindickey@hotmail.co.uk>
2019-10-28 08:03:14 +0000

    What plan? DONE
    Had an idea while doing my other program/critiquing it.
    
    Basically, it was getting too hairy. I was checking here, there, and everywhere what the current state was etc.
    
    In this new way, I just have a main loop that tracks the position of the screen, and goes through every register whether the other bits of the program like it or not. Then, in each iteration, we either set the current register black or white depending on the keypress.
    
    Instead of going back to A each time we detect a change, and detecting changes constantly, we just CONTINUE doing what we're doing, and slightly change the execution if there is/isn't input.
    
    Way more responsive. Done.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -3,73 +3,69 @@ D=A
 @numScreenRegisters	//Number of draw loop iterations required
 M=D		
 
-(SCAN_LOOP)
-	@KBD
+//could just iterate around constantly, and each loop detect keyboard, and depending on that,
+//set next register white or black
+
+//Store number of registers that can be filled
+@8192
+D=A
+@numScreenRegisters	
+M=D	
+
+@iteration
+M=0
+
+(LOOP)
+	//If reached end of screen, reset loop position
+	@numScreenRegisters
 	D=M
-
-	//TODO here: Loop to set white if not pressed
-
-	//if keyboard not pressed, scan again, else continue to draw loop and fill screen 
-	@SCAN_LOOP
+	@iteration
+	D=D-M
+	@RESET_LOOP
 	D;JEQ	
 
-	//If already drawing, skip initialisation of loop counters
-	@iteration
+	//Set black if key pressed, else set white
+	@KBD
 	D=M
-	@numScreenRegisters
-	D=M-D		//D = numScreenRegisters - iteration
-	@DRAW_LOOP	
-	D;JGT		
+	@SET_BLACK
+	D;JGT
+	@SET_WHITE
+	0;JMP
 
+(SET_BLACK)
+	@SCREEN
+	D=A
 	@iteration
-	M=0			
-
-	(DRAW_LOOP)
-		//Multiply 16, "current iteration" many times
-		@registerIncrement
-		M=0
-		@timesRegisterIncremented
-		M=0
-		(INCREMENT_REGISTER)
-			//If iteration == 0, skip loop
-			@iteration
-			D=M
-			@SET_SCREEN
-			D;JEQ
-
-			//Or if iteration == times to set, skip loop
-			@timesRegisterIncremented
-			D=M
-			@iteration
-			D=M-D
-			@SET_SCREEN
-			D;JEQ
-
-			//Else, set register position += 1
-			@1
-			D=A
-			@registerIncrement
-			M=M+D
-
-			//Count num times incremented register and continue
-			@timesRegisterIncremented
-			M=M+1
-			@INCREMENT_REGISTER
-			0;JMP
-		
-		(SET_SCREEN)
-			//Set current register to all black
-			@SCREEN
-			D=A
-			@registerIncrement
-			D=D+M
-			A=D
-			M=-1
+	D=D+M		
+	A=D		//set register position
+	M=-1
 			
-			@iteration	
-			M=M+1
-			D=M
+	@iteration	
+	M=M+1
+	D=M
 
-			//Scan again
-			@SCAN_LOOP
-			0;JMP
+	@LOOP
+	0;JMP
+
+(SET_WHITE)
+	@SCREEN
+	D=A
+	@iteration
+	D=D+M
+	A=D		//set register position
+	M=0
+			
+	@iteration	
+	M=M+1
+	D=M
+
+	@LOOP
+	0;JMP
+
+(RESET_LOOP)
+	@iteration
+	M=0
+	
+	//Return to loop
+	@LOOP
+	0;JMP
