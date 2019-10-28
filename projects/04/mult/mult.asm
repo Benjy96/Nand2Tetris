ce8a2c7c374109c96f46060b1480bddce7af2984
Benjy96 <benjamindickey@hotmail.co.uk>
2019-10-28 08:19:08 +0000

    Complete assembly multiplication program

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,38 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Addition is adding x times
+	//I.e., add R0 to itself R1 times
+
+@R1
+D=M
+@timesToAdd
+M=D
+
+@iteration
+M=1
+
+(LOOP)
+	//Add R0 to itself
+	@R0
+	D=M
+	@R2
+	M=M+D
+	
+	//If added required number of times, end program
+	@timesToAdd
+	D=M
+	@iteration
+	D=D-M
+	@END
+	D;JEQ
+
+	//Else continue loop
+	@iteration
+	M=M+1
+	@LOOP
+	0;JMP
+
+(END)
+	@END
+	0;JMP
